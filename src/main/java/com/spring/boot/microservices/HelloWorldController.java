d4d42df5abb103df12a1e1e5929379203652af68
Krishna <surekrrishna@gmail.com>
2018-09-04 20:17:02 +0530

    added internationalization for us and french

--- src/main/java/com/spring/boot/microservices/HelloWorldController.java
+++ src/main/java/com/spring/boot/microservices/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.spring.boot.microservices;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path = "/hello-world")
 	public String hello() {
@@ -23,6 +31,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
 	public HelloWorldBean helloBeanPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalization")
+	public String helloInternationalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 	
 	

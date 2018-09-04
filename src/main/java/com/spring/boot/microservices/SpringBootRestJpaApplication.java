3b9d338f08337153daa022127eb6fc1507010401
Krishna <surekrrishna@gmail.com>
2018-09-04 20:24:04 +0530

    No need to create LocalResolver bean instead add property [spring.messages.basename=messages] in application.properties

--- src/main/java/com/spring/boot/microservices/SpringBootRestJpaApplication.java
+++ src/main/java/com/spring/boot/microservices/SpringBootRestJpaApplication.java
@@ -1,27 +1,15 @@
 package com.spring.boot.microservices;
 
-import java.util.Locale;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SpringBootRestJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestJpaApplication.class, args);
-	}
-	
-	
-	@Bean
-	public LocaleResolver localeResolver() {
-		var slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US);
-		return slr;		
 	}
 	
 	@Bean

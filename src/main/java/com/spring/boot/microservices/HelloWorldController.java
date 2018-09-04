e387d7f810fb61185512d0f35dc6666e288276dd
Krishna <surekrrishna@gmail.com>
2018-09-04 20:26:41 +0530

    Remove @RequestHeader instead add LocaleContextHolder.getLocale()

--- src/main/java/com/spring/boot/microservices/HelloWorldController.java
+++ src/main/java/com/spring/boot/microservices/HelloWorldController.java
@@ -1,12 +1,10 @@
 package com.spring.boot.microservices;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,8 +32,8 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path = "/hello-world-internationalization")
-	public String helloInternationalization(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloInternationalization() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	
 	
